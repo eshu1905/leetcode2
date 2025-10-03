f78724d34934ff0b6ce1d9b90e7b5e70fe906af7
G.easwer <garimellaeshear@gmail.com>
2025-10-03 09:41:39 +0530

    Time: 32 ms (95.93%) | Memory: 53.9 MB (40.68%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let len=arr.length;
+    let jas=[];
+    for(let i=0;i<len;i++){
+        jas[i]=(fn(arr[i],i))
+        }
+    return jas;    
+    
+};
