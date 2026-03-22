ede2698e6bbc50699016494a4dcbb7cefc1a55bc
G.easwer <garimellaeshear@gmail.com>
2026-03-22 09:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (97.22%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -4,16 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution(object):
     def isSameTree(self, p, q):
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        if p.val!=q.val:
-            return False
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-
+        queue = deque([(p, q)])
+    
+        while queue:
+            n1, n2 = queue.popleft()
+        
+            if not n1 and not n2:
+                continue
+            if not n1 or not n2:
+                return False
+            if n1.val != n2.val:
+                return False
+        
+            queue.append((n1.left, n2.left))
+            queue.append((n1.right, n2.right))
+    
+        return True
+    
         """
         :type p: Optional[TreeNode]
         :type q: Optional[TreeNode]
