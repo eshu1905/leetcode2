e59f8c34027aac7e16f9286754fbe7efaf358583
G.easwer <garimellaeshear@gmail.com>
2026-03-31 09:06:44 +0530

    Time: 83 ms (80.42%) | Memory: 22.4 MB (81.67%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -45,4 +45,4 @@ class Codec:
             i += 1
 
         return root
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+#__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
